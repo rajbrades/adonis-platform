f6677df1581c7f71e7049da8c6c5b65f099c834b
Ryan Brady <ryanbrady@Ryans-Mac-Studio.local>
2025-11-08 08:59:50 -0500

    fix: Exclude backup directories from build

--- backups/adonis-platform-20251102-171854/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs'
-
-export default function SignInPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-black flex items-center justify-center p-6">
-      <SignIn />
-    </div>
-  )
-}
--- backups/adonis-platform-20251102-171854/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs'
-
-export default function SignUpPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-black flex items-center justify-center p-6">
-      <SignUp />
-    </div>
-  )
-}
